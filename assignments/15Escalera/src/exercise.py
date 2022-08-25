468e95199c69af3ace2c481fe09640a864324dea
sofilopez <A01412110@tec.mx>
2022-08-25 09:13:29 -0500

    sos

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,7 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    alt = int(input("Altura de la casa: "))
+    ang = float(input("Angulo en grados: "))
+    ang = float(math.sin(math.radians(ang)))
+    large = round(alt/ang)
+    print("Largo de la escalera:" , large)
 
 if __name__ == '__main__':
     main()
